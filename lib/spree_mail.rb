4ae8bc68af13591bfce93521973c5ed544f95b46
kennyadsl <kennyadsl@gmail.com>
2011-01-29 10:08:47 -0800

    tryng to fix middleware error in production

--- lib/spree_mail.rb
+++ lib/spree_mail.rb
@@ -9,7 +9,7 @@ module SpreeMail
     config.autoload_paths += %W(#{config.root}/lib)
 
     initializer "static assets" do |app|
-      app.middleware.insert_after ::ActionDispatch::Static, ::ActionDispatch::Static, "#{config.root}/public"
+      app.middleware.insert_before ::Rack::Lock, ::ActionDispatch::Static, "#{config.root}/public"
     end
 
     #def self.activate
