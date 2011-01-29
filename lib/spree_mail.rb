898a7fdc61666c007418d0cfbe958f7b020fa488
kennyadsl <kennyadsl@gmail.com>
2011-01-29 10:26:36 -0800

    delete middleware insertion

--- lib/spree_mail.rb
+++ lib/spree_mail.rb
@@ -8,9 +8,9 @@ module SpreeMail
 
     config.autoload_paths += %W(#{config.root}/lib)
 
-    initializer "static assets" do |app|
-      app.middleware.insert_before ::Rack::Lock, ::ActionDispatch::Static, "#{config.root}/public"
-    end
+    #initializer "static assets" do |app|
+    #  app.middleware.insert_before ::Rack::Lock, ::ActionDispatch::Static, "#{config.root}/public"
+    #end
 
     #def self.activate
     #  #Dir["../app/**/*.rb"].each do |c|
